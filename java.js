ba18c7bcd94291a167c8ebb72368f6c635f04dc9
MohammedShtewi11 <78658878+MohammedShtewi11@users.noreply.github.com>
2021-04-30 00:16:28 +0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,73 @@
+
+    var Fname = document.getElementById('fname').value;
+    var lname = document.getElementById('lname').value;
+    var Pwebsit = document.getElementById('Pwebsit').value;
+    var P_num = document.getElementById('P_num').value;
+    var Email = document.getElementById('Email').value;
+    var Username = document.getElementById('Username').value;
+    var pass = document.getElementById('pass').value;
+    var Degree = document.getElementById('Degree').value;
+    var T_graduate = document.getElementById('T_graduate').value;
+
+function show_err() {
+
+    if (Fname === "") {
+        alert("Plz enter your first name to complete");
+    }
+    if (lname === "") {
+        alert("Plz enter your last name to complete");
+    }
+
+    if (Pwebsit === "") {
+        alert("Plz enter your websit name to complete");
+    }
+    if (P_num === "") {
+        alert("Plz enter your number to complete");
+    }
+    
+    if(Email  === '') {
+        alert("Plz enter your Email to complete");
+
+		
+	} 
+    else if (!isEmail(Email)) {
+        alert("Plz enter your Email to complete");
+    }
+
+    if (Username === "") {
+        alert("Plz enter your Username to complete");
+    }
+    if (pass === "") {
+        alert("Plz enter your password to complete");
+    }
+    if (Degree === "") {
+        alert("Plz enter your Degree to complete");
+    }
+
+    if (T_graduate === "") {
+        alert("Plz enter your T_graduate to complete");
+    }
+
+}
+
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
+
+function save() {
+    alert("The application of ["+Email.value+"] is saved!");
+}
+
+
+function Reset() {
+    Fname.value=''
+    lname.value=''
+    Pwebsit.value=''
+    P_num.value=''
+    Email.value=''
+    Username.value=''
+    pass.value=''
+    Degree.value=''
+    T_graduate.value=''
+}
